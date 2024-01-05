348b29dbe3f71fba6d8527078b94391fd15537ef
imran-hossain <engg.imran22@gmail.com>
2024-01-05 13:03:58 +0600

    database connection setup

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ app.use(express.json())
 
 // database connection
 
-
+mongoose.connect("mongodb://localhost:27017/todos")
+       .then(()=>console.log("connection successfullay"))
+    .catch((err)=>console.log(err))
 
 function erroHandaler(err,req,res,next){
     if(res.headersSent){
